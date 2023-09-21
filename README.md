2638a12b9442d30d79798a4bc2fb17a3522b0ec0
ayanrahatali786 <86987476+ayanrahat786@users.noreply.github.com>
2023-09-22 03:24:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# QnA-forum-for-university-students
-The QnA (questions and answers) forum feature provides an area for community members to ask and answer questions. It allows members to: Create new questions. Add inline images (with support for drag-and-drop)
+# MBMDiscuss Guide section
+This is a discussion forum like quora but it is divided by categories and specially design for college students. It is an initiative to help the upcoming students with the discussion forum. Mbmdiscuss focuses on providing the most efficient way to solve your queries or solution wants to be simple way. We want to connect the people who have knowledge to the people who need it..
+
+features
+->Login system
+->User profile section
+->Category creation with database
+->Thread system - creatin threads and you can reply to any thread
+
+Technology used
+Frontend -> Html5/CSS3/Js/Bottstrap
+BackEnd -> PHP
+
+
+![Screenshot (58)](https://user-images.githubusercontent.com/65109464/102325315-045a9400-3fa9-11eb-9abc-37d2bcc24b15.png)
+![Screenshot (59)](https://user-images.githubusercontent.com/65109464/102325373-12a8b000-3fa9-11eb-8937-84976a265821.png)
+![Screenshot (60)](https://user-images.githubusercontent.com/65109464/102325417-1d634500-3fa9-11eb-8de6-febd3843b1b0.png)
+![Screenshot (61)](https://user-images.githubusercontent.com/65109464/102325432-22c08f80-3fa9-11eb-9401-4091d7c12163.png)
+![Screenshot (62)](https://user-images.githubusercontent.com/65109464/102325441-26ecad00-3fa9-11eb-9ec6-d21aaebe7e43.png)
+![Screenshot (63)](https://user-images.githubusercontent.com/65109464/102325453-29e79d80-3fa9-11eb-959c-129449fd9d2a.png)
+![Screenshot (64)](https://user-images.githubusercontent.com/65109464/102325467-2e13bb00-3fa9-11eb-9530-0a9a292f366c.png)
+![Screenshot (65)](https://user-images.githubusercontent.com/65109464/102325474-31a74200-3fa9-11eb-9cde-63c4086861a2.png)
+![Screenshot (66)](https://user-images.githubusercontent.com/65109464/102325485-34a23280-3fa9-11eb-8717-2f93b9043a53.png)
+![Screenshot (67)](https://user-images.githubusercontent.com/65109464/102325497-37048c80-3fa9-11eb-8413-b98e4fa39c26.png)
---
+++ about.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+  <title>Welcome to iDiscuss-coding forums</title>
+  <style>
+    section{
+      height: 70vh;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include 'partial/_header.php'; ?>
+
+  <main role="main">
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item">
+    <img src="img6.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
+      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
+      <!-- <div class="container">
+        <div class="carousel-caption text-left">
+          <h1>Example headline.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+        </div>
+      </div> -->
+      <div class="carousel-caption d-none d-md-block">
+                    <!-- <h1>Welcome</h1> -->
+                    <h1>Our Team</h1>
+          </div>
+    </div>
+    <div class="carousel-item">
+    <img src="img8.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
+      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
+      <!-- <div class="container">
+        <div class="carousel-caption">
+          <h1>Another example headline.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+        </div>
+      </div> -->
+      <div class="carousel-caption d-none d-md-block">
+                    <!-- <h1>Welcome</h1> -->
+                    <h1>Our Team</h1>
+          </div>
+    </div>
+    <div class="carousel-item active">
+    <img src="img7.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
+      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
+      <!-- <div class="container">
+        <div class="carousel-caption text-right">
+          <h1>One more for good measure.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+        </div>
+      </div> -->
+      <div class="carousel-caption d-none d-md-block">
+                    <!-- <h1>Welcome</h1> -->
+                    <h1>Our Team</h1>
+          </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<!-- Marketing messaging and featurettes
+================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+
+<div class="container marketing bg-light">
+<hr class="featurette-divider">
+ 
+
+  <!-- Three columns of text below the carousel -->
+
+  <div class="text-center">
+  <hr class="w-100 mx-auto">
+            <h1 class="display-4"><b>About Us</b></h1>
+            <hr class="w-100 mx-auto">
+        </div>
+        <div class="container">
+            <div class="row my-5">
+                <div class="col-lg-6 col-md-6 col-12 col-xxl-6">
+                    <figure>
+                      <img src="dp.jpg" alt="">
+                    </figure>
+                </div>
+                <div
+                    class="col-lg-6 col-md-6 col-12 col-xxl-6 d-flex justify-content-center align-items-start flex-column" style="color:white">
+                    <h1>My journey</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus minus corrupti quos nostrum
+                        at eveniet ut illum commodi voluptate? Mollitia autem dicta ut earum odit nam neque, soluta
+                        natus obcaecati, amet ullam dolorem, illum velit numquam magnam veritatis voluptates quia. </p>
+                    <button type="button" class="btn btn-outline-info" data-toggle="tooltip" data-placement="right"
+                        title="Who am i?">Cheak more</button>
+                </div>
+            <div class="row my-5">
+               
+                <div class="col-lg-6 col-md-6 col-12 col-xxl-6 d-flex justify-content-center align-items-start flex-column">
+                    <h1>My journey</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus minus corrupti quos nostrum
+                        at eveniet ut illum commodi voluptate? Mollitia autem dicta ut earum odit nam neque, soluta
+                        natus obcaecati, amet ullam dolorem, illum velit numquam magnam veritatis voluptates quia. </p>
+                    <button type="button" class="btn btn-outline-info" data-toggle="tooltip" data-placement="right"
+                        title="Who am i?">Cheak more</button>
+                </div>
+                <div class="col-lg-6 col-md-6 col-12 col-xxl-6">
+                    <figure>
+                        <img src="mbm1.jpg" alt="" class="img-fluid" id="about-img">
+                    </figure>
+                </div>
+            </div>
+
+  <!-- START THE FEATURETTES -->
+
+ 
+  <hr class="featurette-divider">
+
+  <!-- /END THE FEATURETTES -->
+
+</div><!-- /.container -->
+
+
+<!-- FOOTER -->
+<!-- <footer class="container">
+  <p class="float-right"><a href="#">Back to top</a></p>
+  <p>© 2017-2020 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+</footer> -->
+</main>
+     <?php include 'partial/footer.php';  ?>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
